6c93adfeae74eb8bc8193b163bb8ed31fccd5d43
p0186797 <76880486+p0186797@users.noreply.github.com>
2022-12-09 10:03:49 -0500

    Add files via upload

---
+++ tictactoe.py
@@ -0,0 +1,17 @@
+def create_board():
+    '''Create an empty nested list 3 by 3.'''
+
+def play(marker, row, col, board):
+    '''A marker is either x or o. A row is an int 0 - 2. 
+    A col is an int 0 - 2. Places marker on board at row and col.
+    Returns the updated board.'''
+
+def print_board(board):
+    '''Prints the board so it looks like a tic-tac-toe board.'''
+
+def is_there_a_winner(board):
+    '''Returns true there is a winner.'''
+
+def get_row_col(board):
+    '''Get row and col from user. Returns row and col as tupple.'''
+
